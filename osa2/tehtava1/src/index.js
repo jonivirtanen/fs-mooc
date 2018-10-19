4fdfa30b3ea2e9cc57abd44f1bfaa8ce2bc63763
Joni Virtanen <vijoni91@gmail.com>
2018-10-19 21:58:36 +0300

    tehtava2-3

--- osa2/tehtava1/src/index.js
+++ osa2/tehtava1/src/index.js
@@ -7,6 +7,7 @@ const Kurssi = ({kurssi}) => {
     <div>
         <Otsikko otsikko={kurssi.nimi} />
         <Sisalto osat={kurssi.osat} />
+        <Yhteensa osat={kurssi.osat} />
     </div>
     )
 }
@@ -24,18 +25,14 @@ const Sisalto = ({osat}) => {
 
 const Osa = ({osa}) => (<li>{osa.nimi} {osa.tehtavia}</li>)
 
-const Yhteensa = (props) => {
-    let yhteensa = 0
+const Yhteensa = ({osat}) => {
+    let yhteensa = osat.reduce((yhteensa, osa) => yhteensa + osa.tehtavia, 0)
     
-    props.osat.forEach(element => {
-        yhteensa += element.tehtavia
-    });
-
     return (  
       <div>
-        <p>Yhteensä {yhteensa}</p>
+        <p>Yhteensä {yhteensa} tehtavaa</p>
       </div>
-  )
+    )
 }
 
 const App = () => {
