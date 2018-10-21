c08ffddbaa227e161a2d3dab5a9467f34257dd21
Joni Virtanen <vijoni91@gmail.com>
2018-10-22 00:03:15 +0300

    Osa3

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,34 +1,48 @@
 import React from 'react';
 
 const DisplayPerson = ({person}) => (
-    <p>{person.name}</p>
+    <tr>
+        <td>{person.name}</td>
+        <td>{person.phone}</td>
+    </tr>
 )
 class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
       persons: [
-        { name: 'Arto Hellas' }
+        { 
+          name: 'Arto Hellas',
+          phone: '040-123456'
+        }
       ],
-      newName: ''
+      newName: '',
+      newPhone: ''
     }
   }
   addPerson = (event) => {
       event.preventDefault()
       const personObject = {
-          name: this.state.newName
+          name: this.state.newName,
+          phone: this.state.newPhone
       }
       if(this.state.persons.findIndex(person => person.name === personObject.name) === -1) {
         const persons = this.state.persons.concat(personObject)
+        
         this.setState({
-          persons: persons,
-          newName: ''
+          persons,
+          newName: '',
+          newPhone: ''
         })
       }
   }
 
   handleNameChange = (event) => {
     this.setState({newName: event.target.value})
+  }
+
+  handleNumberChange = (event) => {
+      this.setState({newPhone: event.target.value})
   }
 
   render() {
@@ -40,11 +54,18 @@ class App extends React.Component {
             nimi: <input onChange={this.handleNameChange}/>
           </div>
           <div>
+            puhelinnumero: <input onChange={this.handleNumberChange} />
+          </div>
+          <div>
             <button type="submit">lisää</button>
           </div>
         </form>
         <h2>Numerot</h2>
-        {this.state.persons.map(person => <DisplayPerson key={person.name} person={person} />)}
+        <table>
+          <tbody>
+            {this.state.persons.map(person => <DisplayPerson key={person.name} person={person} />)}
+          </tbody>
+        </table>
       </div>
     )
   }
