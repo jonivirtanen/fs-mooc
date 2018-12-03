79c566b8f959f7ce3dd7a136d294642daae7e4b9
Joni Virtanen <vijoni91@gmail.com>
2018-12-03 13:24:49 +0200

    5.15-5.16

--- bloglist-frontend/src/tests/SimpleBlog.test.js
+++ bloglist-frontend/src/tests/SimpleBlog.test.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import { shallow } from 'enzyme'
-import SimpleBlog from '../SimpleBlog'
+import SimpleBlog from '../components/SimpleBlog'
 
-describe.only('<SimpleBlog />', () => {
+describe('<SimpleBlog />', () => {
   const blog = {
     title: 'Testausta',
     author: 'Masa Mainio',
