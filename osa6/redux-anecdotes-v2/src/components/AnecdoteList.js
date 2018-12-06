e886c5fb89635c7491f5da1c5ed7e0d33b735b33
Joni Virtanen <vijoni91@gmail.com>
2018-12-06 18:44:45 +0200

    6.9

--- osa6/redux-anecdotes-v2/src/components/AnecdoteList.js
+++ osa6/redux-anecdotes-v2/src/components/AnecdoteList.js
@@ -5,42 +5,43 @@ import { connect } from 'react-redux'
 
 class AnecdoteList extends React.Component {
   render() {
-    console.log(this.props)
-    const { anecdotes, filter } = this.props
-
-    const results = anecdotes.filter(a => a.content.includes(filter))
     return (
       <div>
         <h2>Anecdotes</h2>
-        {results.sort((a, b) => b.votes - a.votes).map(anecdote =>
-          <div key={anecdote.id}>
-            <div>
-              {anecdote.content}
-            </div>
-            <div>
-              has {anecdote.votes}
-              <button onClick={() => {
-                this.props.voteAnecdote(anecdote.id)
-                this.props.setNotification(`You voted for ${anecdote.content}`)
-                setTimeout(() => {
-                  this.props.removeNotification()
-                }, 5000);
-              }
-              }>
-                vote
-              </button>
-            </div>
-          </div>
-        )}
+        {this.props.anecdotes
+          .sort((a, b) => b.votes - a.votes)
+            .map(anecdote =>
+              <div key={anecdote.id}>
+                <div>
+                  {anecdote.content}
+                </div>
+                <div>
+                  has {anecdote.votes}
+                  <button onClick={() => {
+                    this.props.voteAnecdote(anecdote.id)
+                    this.props.setNotification(`You voted for ${anecdote.content}`)
+                    setTimeout(() => {
+                      this.props.removeNotification()
+                    }, 5000);
+                  }
+                  }>
+                    vote
+                  </button>
+                </div>
+              </div>
+              )
+        }
       </div>
     )
   }
 }
 
+const anecdotesToShow = (anecdotes, filter) => {
+  return anecdotes.filter(a => a.content.includes(filter))
+}
 const mapStateToProps = (state) => {
   return { 
-    anecdotes: state.anecdotes,
-    filter: state.filter
+    anecdotes: anecdotesToShow(state.anecdotes, state.filter)
    }
 }
 
